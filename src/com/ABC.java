b829dba56e3a53411e7cc0e3e121a65cfd5108b9
root <2178127391@qq.com>
2022-04-02 11:43:13 +0800

    修改ABC类,增加多行dev

--- src/com/ABC.java
+++ src/com/ABC.java
@@ -4,5 +4,10 @@ public class ABC {
     public static void main(String[] args) {
         System.out.println("I am master分支11111111");
         System.out.println("I am dev分支222222");
+        System.out.println("I am dev分支222222");
+        System.out.println("I am dev分支222222");
+        System.out.println("I am dev分支222222");
+        System.out.println("I am dev分支222222");
+        System.out.println("I am dev分支222222");
     }
 }
